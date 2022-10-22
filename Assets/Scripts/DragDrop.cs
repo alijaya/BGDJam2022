870c60426fa393cada91f24b0b8dcca42afecd38
Ali Jaya Meilio <alijayameilio@gmail.com>
2022-10-22 12:14:39 +0700

    refactor

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
 
-    [SerializeField] private Canvas canvas;
+    private Canvas canvas;
 
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
@@ -14,6 +14,7 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
         initPosition = rectTransform.anchoredPosition;
     }
 
