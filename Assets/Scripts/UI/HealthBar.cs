ad3c5f12a4710e761cb3b151ea64c5b2604d8819
Ali Jaya Meilio <alijayameilio@gmail.com>
2022-10-22 20:41:04 +0700

    HP HUD

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityAtoms.BaseAtoms;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private RectTransform fillBar;
+
+    public IntReference currentHealth;
+    public IntReference maxHealth;
+
+    private IntEvent currentHealthChanged;
+    private IntEvent maxHealthChanged;
+    void Awake()
+    {
+        currentHealthChanged = currentHealth.GetEvent<IntEvent>();
+        maxHealthChanged = maxHealth.GetEvent<IntEvent>();
+    }
+
+    private void OnEnable()
+    {
+        currentHealthChanged.Register(UpdateDisplay);
+        maxHealthChanged.Register(UpdateDisplay);
+        UpdateDisplay();
+    }
+
+    private void OnDisable()
+    {
+        currentHealthChanged.Unregister(UpdateDisplay);
+        maxHealthChanged.Unregister(UpdateDisplay);
+    }
+
+    public void UpdateDisplay()
+    {
+        text.text = currentHealth.Value + " / " + maxHealth.Value;
+        var percentage = (float)currentHealth.Value / maxHealth.Value;
+        Debug.Log(percentage);
+        fillBar.anchorMax = new Vector2(percentage, fillBar.anchorMax.y);
+        fillBar.anchorMin = new Vector2(percentage - 1, fillBar.anchorMin.y);
+    }
+
+
+}
