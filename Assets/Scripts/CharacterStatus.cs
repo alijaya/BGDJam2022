01914741c6f1438a7bace29f3af0ffc2a8ceda07
Ali Jaya Meilio <alijayameilio@gmail.com>
2022-10-23 01:06:40 +0700

    Harusnya dah bisa attack

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -8,10 +8,15 @@ public class CharacterStatus : ScriptableObject
 {
     public IntReference currentHP;
     public IntReference maxHP;
+    public VoidBaseEventReference deathEvent;
 
     public void DoDamaged(int damage)
     {
         currentHP.Value -= damage;
+        if (currentHP.Value <= 0)
+        {
+            deathEvent.Event.Raise();
+        }
     }
 
     public void DoHealed(int damage)
--- Assets/Scripts/SlotMB.cs
+++ Assets/Scripts/SlotMB.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class SlotMB : MonoBehaviour, IDropHandler
 {
     private SkillMB skill;
     private CardSkill cardSkill;
 
+    public TextMeshProUGUI text;
+
     void Awake()
     {
         skill = GetComponentInParent<SkillMB>();
         cardSkill = skill.cardSkill;
+    }
+
+    void Start()
+    {
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        switch (cardSkill.requirementType)
+        {
+            case CardSkill.RequirementType.Normal: text.text = ""; break;
+            case CardSkill.RequirementType.Exact: text.text = "" + cardSkill.requirementValue; break;
+            case CardSkill.RequirementType.Max: text.text = "Max " + cardSkill.requirementValue; break;
+            case CardSkill.RequirementType.Min: text.text = "Min " + cardSkill.requirementValue; break;
+            case CardSkill.RequirementType.Odd: text.text = "Odd"; break;
+            case CardSkill.RequirementType.Even: text.text = "Even"; break;
+            default: text.text = ""; break;
+        }
     }
 
     public void OnDrop(PointerEventData eventData)
@@ -24,13 +46,11 @@ public class SlotMB : MonoBehaviour, IDropHandler
 
             var playerStatus = GlobalRef.instance.playerStatus;
             var enemyStatus = GlobalRef.instance.enemyStatus;
-            cardSkill.DoActionWithTest(roll, playerStatus, enemyStatus);
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (cardSkill.DoActionWithTest(roll, playerStatus, enemyStatus))
+            {
+                Debug.Log("Attack!");
+            }
+        }
     }
 }
